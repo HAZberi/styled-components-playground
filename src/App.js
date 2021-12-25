91dfda916bde48f90cfc74d9037ee445f57edd76
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-25 14:46:54 +0300

    Added Header Component Changed body margin to 0 Tested nested styles Styles integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 function App() {
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    padding: 25px 0;
+    background-color: #ebfbff;
+
+    h1 {
+        color: red;
+    }
+
+    &:hover {
+        background-color: black;
+    }
+`
