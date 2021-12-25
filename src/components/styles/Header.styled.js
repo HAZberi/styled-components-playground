290ba7187a89ca3359a3645c0072b6cd01af9584
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-25 16:46:27 +0300

    Nav, Logo and Button components for Header created

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -4,3 +4,12 @@ export const StyledHeader = styled.header`
   padding: 25px 0;
   background-color: ${({ theme }) => theme.colors.header};
 `;
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+`
+
+export const Logo = styled.img``;
