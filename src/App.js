207d59158aaf07d062420389da1f535ecf8b2f44
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-25 15:02:05 +0300

    Props and Theme Provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Header from "./components/Header";
+import { ThemeProvider } from "styled-components";
 import { Container } from "./components/styles/Container.styled";
 function App() {
+  const theme = {
+    colors: {
+      header: '#ebfbff',
+      body: '#fff',
+      footer: '#003333',
+    }
+  }
   return (
-    <>
-      <Header />
-      <Container>
-        <h1>Hello World</h1>
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container pd='0px'>
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-    padding: 25px 0;
-    background-color: #ebfbff;
-
-    h1 {
-        color: red;
-    }
-
-    &:hover {
-        background-color: black;
-    }
-`
+  padding: 25px 0;
+  background-color: ${({ theme }) => theme.colors.header};
+`;
